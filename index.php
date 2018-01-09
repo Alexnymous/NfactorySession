ce5257b8cf45cee9bf4c2be1576efac2a7a2123f
Alexnymous <32875309+Alexnymous@users.noreply.github.com>
2018-01-09 17:21:57 +0100

    creation session bidon

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 09/01/2018
+ * Time: 17:07
+ */session_start();
+ $_SESSION ['Nom'] = "Dupont";
+ $_SESSION ['Prenom'] = "Ducon";
+ $_SESSION ['age'] = 69;
